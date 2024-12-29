da0de53cacfde13f2322d754e63ca00420eb94dc
Pocholo Armendi <47001647+poch20@users.noreply.github.com>
2024-12-30 07:39:09 +0800

    Update gallery.js
    Relate to Spotify

--- staticfiles/js/gallery.js
+++ staticfiles/js/gallery.js
@@ -1,14 +1,12 @@
-const spotifyPlayer = document.getElementById('spotify-player');
+const spotifyPlayer = document.querySelector('iframe');
 const musicButton = document.getElementById('music-button');
 
 function toggleMusic() {
-  if (spotifyPlayer.style.display === 'none' || spotifyPlayer.style.display === '') {
-    spotifyPlayer.style.display = 'block';
-    musicButton.textContent = 'Pause Spotify';
-  } else {
-    spotifyPlayer.style.display = 'none';
-    musicButton.textContent = 'Play Lo-Fi';
-  }
+    if (spotifyPlayer.style.display === 'none' || spotifyPlayer.style.display === '') {
+        spotifyPlayer.style.display = 'block';
+        musicButton.textContent = 'Pause Lo-Fi';
+    } else {
+        spotifyPlayer.style.display = 'none';
+        musicButton.textContent = 'Play Lo-Fi';
+    }
 }
-
-musicButton.addEventListener('click', toggleMusic);
