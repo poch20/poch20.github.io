ede89f4474fa9f9f60316e78b994c921ffb7695e
Pocholo Armendi <47001647+poch20@users.noreply.github.com>
2024-12-30 07:48:00 +0800

    Fixing nasira e

--- staticfiles/js/gallery.js
+++ staticfiles/js/gallery.js
@@ -1,12 +1,12 @@
-const spotifyPlayer = document.querySelector('iframe');
-const musicButton = document.getElementById('music-button');
+const spotifyPlayer = document.getElementById("spotify-player");
+const musicButton = document.getElementById("music-button");
 
 function toggleMusic() {
-    if (spotifyPlayer.style.display === 'none' || spotifyPlayer.style.display === '') {
-        spotifyPlayer.style.display = 'block';
-        musicButton.textContent = 'Pause Lo-Fi';
-    } else {
-        spotifyPlayer.style.display = 'none';
-        musicButton.textContent = 'Play Lo-Fi';
-    }
+  if (spotifyPlayer.style.display === "none" || spotifyPlayer.style.display === "") {
+    spotifyPlayer.style.display = "block";
+    musicButton.textContent = "Pause Lo-Fi";
+  } else {
+    spotifyPlayer.style.display = "none";
+    musicButton.textContent = "Play Lo-Fi";
+  }
 }
