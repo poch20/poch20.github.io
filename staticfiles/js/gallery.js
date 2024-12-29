841f5085f664c3b70f416e4d31cd06442116cfe1
Pocholo Armendi <47001647+poch20@users.noreply.github.com>
2024-12-30 00:11:04 +0800

    Update gallery.js

--- staticfiles/js/gallery.js
+++ staticfiles/js/gallery.js
@@ -1,5 +1,15 @@
-document.querySelectorAll('.gallery-container img').forEach(image => {
-  image.addEventListener('click', () => {
-    alert('This feature will expand soon!');
-  });
+// Toggle Music Functionality
+const audio = document.getElementById('bgMusic');
+const musicToggle = document.getElementById('musicToggle');
+let isPlaying = false;
+
+musicToggle.addEventListener('click', () => {
+    if (isPlaying) {
+        audio.pause();
+        musicToggle.textContent = "Play Music";
+    } else {
+        audio.play();
+        musicToggle.textContent = "Pause Music";
+    }
+    isPlaying = !isPlaying;
 });
